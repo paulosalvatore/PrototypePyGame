02322befd5e7cf980b01c61f11809686b255089f
paulosalvatore <paulo.salvatore@hotmail.com.br>
2016-03-29 09:09:58 -0300

    Diversas alterações
    Mudanças no código base do código
    Câmera adicionada
    Novos tiles adicionados e alguns foram removidos ou modificados

--- main.py
+++ main.py
@@ -1,246 +1,390 @@
 
-import pygame
 import os
 import sys
 import random
+import pygame as pg
 
-os.environ['SDL_VIDEO_CENTERED'] = '1'
-
-pygame.init()
-
-tamanhoBordaExterna = 32
-tamanhoAdicionalBordaExterna = tamanhoBordaExterna / 2
-tamanhoTile = 64
-tamanhoAdicional = tamanhoBordaExterna + tamanhoTile / 2
-tilesJogo = [7, 12]
-tilesBorda = [2, 2]
-tiles = [tilesJogo[1]+(tilesBorda[1]*2), tilesJogo[0]+(tilesBorda[0]*2)]
-
-posicaoInicialJogador = {"x": 6, "y": 0}
-
-posicaoJogador = [
-	posicaoInicialJogador["x"]+1,
-	posicaoInicialJogador["y"]+2
-]
-
-mapa = [
-	[2, 3, 0, 0, 0, 0, 0, 0, 1, 0, 3, 2],
-	[3, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 3],
-	[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-	[3, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 3],
-	[2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2]
-]
-
-configTiles = {
-	0: {
-		"block": False,
-		"nomeArquivo": "tile"
+config = {
+	"janela": {
+		"titulo": "Jogo",
+		"tamanho": [0, 0],
+		"corFundo": (0, 0, 0)
 	},
-	1: {
-		"block": True,
-		"nomeArquivo": "parede"
+	"jogo": {
+		"area": (28, 18),
+		"areaJanela": (14, 9)
 	},
-	2: {
-		"block": False,
-		"nomeArquivo": "tile2"
+	"tiles": {
+		"tamanho": 64,
+		"tipos": {
+			0: {
+				"nomeArquivo": "piso",
+				"block": False,
+				"arquivo": ""
+			},
+			1: {
+				"nomeArquivo": "parede",
+				"block": True,
+				"arquivo": ""
+			},
+			2: {
+				"nomeArquivo": "porta_fechada",
+				"block": True,
+				"arquivo": ""
+			},
+			3: {
+				"nomeArquivo": "porta_aberta",
+				"block": False,
+				"arquivo": ""
+			},
+			4: {
+				"nomeArquivo": "buraco",
+				"block": False,
+				"arquivo": ""
+			}
+		}
 	},
-	3: {
-		"block": False,
-		"nomeArquivo": "tile3"
-	}
+	"jogador": {
+		"nomeArquivo": "skelly",
+		"arquivo": "",
+		"tamanho": [50, 50]
+	},
+	"direcoes": {
+		"diretas": {
+			pg.K_LEFT: (-1, 0),
+			pg.K_RIGHT: ( 1, 0),
+			pg.K_UP: ( 0,-1),
+			pg.K_DOWN: ( 0, 1)
+		},
+		"opostas": {
+			pg.K_LEFT: "right",
+			pg.K_RIGHT: "left",
+			pg.K_UP: "bottom",
+			pg.K_DOWN: "top"
+		}
+	},
+	"mapa": [
+		[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+		[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+		[1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+		[1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+		[1, 1, 0, 2, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+		[1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+		[1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+		[1, 1, 0, 4, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+		[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+		[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+		[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+		[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+		[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+		[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+		[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+		[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+		[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+		[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+	]
 }
 
-tamanhoTela = (int(tiles[0]*tamanhoTile+tamanhoBordaExterna*2), int(tiles[1]*tamanhoTile+tamanhoBordaExterna*2))
+def definirTamanhoJanela():
+	configJogo = config["jogo"]
+	areaJanela = configJogo["areaJanela"]
+	tamanhoTile = config["tiles"]["tamanho"]
+	config["janela"]["tamanho"][0] = (areaJanela[0]) * tamanhoTile
+	config["janela"]["tamanho"][1] = (areaJanela[1]) * tamanhoTile
 
-screen = pygame.display.set_mode(tamanhoTela)
+def calcularPosicao(x, y):
+	tamanhoTile = config["tiles"]["tamanho"]
+	posicaoX = x * tamanhoTile
+	posicaoY = y * tamanhoTile
+	return (posicaoX, posicaoY)
 
-jogo = []
+def checarPosicao(w, h):
+	print(w, h)
+	# tamanhoTile = config["tiles"]["tamanho"]
+	# posicaoX = x * tamanhoTile
+	# posicaoY = y * tamanhoTile
+	# return (posicaoX, posicaoY)
 
-for x in range(tiles[0]):
-	jogo.insert(x, [])
-	for y in range(tiles[1]):
-		if ((x < 2) or
-			(y < 2) or
-			(x > tiles[0] - 3) or
-			(y > tiles[1] - 3)):
-			valor = 1
+def carregarImagens(arquivo):
+	return pg.image.load(os.path.join(sys.path[0], "imagens/"+arquivo+".png")).convert_alpha()
+
+def carregarImagensTiles():
+	for i in range(len(config["tiles"]["tipos"])):
+		nomeArquivo = config["tiles"]["tipos"][i]["nomeArquivo"]
+		config["tiles"]["tipos"][i]["arquivo"] = carregarImagens(nomeArquivo)
+
+def carregarImagemJogador():
+	config["jogador"]["arquivo"] = carregarImagens(config["jogador"]["nomeArquivo"])
+
+class Player(pg.sprite.Sprite):
+	def __init__(self, rect, speed, direction = pg.K_RIGHT):
+		pg.sprite.Sprite.__init__(self)
+		self.rect = pg.Rect(rect)
+		self.mask = self.make_mask()
+		self.speed = speed
+		self.direction = direction
+		self.collision_direction = None
+		self.first_collision_per_frame = None
+		self.old_direction = None
+		self.direction_stack = []
+		self.redraw = False
+		self.image = None
+		self.frame  = 0
+		self.frames = self.get_frames()
+		self.animate_timer = 0.0
+		self.animate_fps = 7.0
+		self.walkframes = []
+		self.walkframe_dict = self.make_frame_dict()
+		self.adjust_images()
+
+		self.frame_start_pos = None
+		self.total_displacement = None
+		self.speed = speed
+
+		self.velocity = [0, 0]
+
+	def make_mask(self):
+		mask_surface = pg.Surface(self.rect.size).convert_alpha()
+		mask_surface.fill((0,0,0,0))
+		mask_surface.fill(pg.Color("white"), (10,20,30,30))
+		mask = pg.mask.from_surface(mask_surface)
+		return mask
+
+	def get_frames(self):
+		indices = [[0,0],[1,0],[2,0],[3,0]]
+		return get_images(config["jogador"]["arquivo"], indices, self.rect.size)
+
+	def make_frame_dict(self):
+		frames = {pg.K_LEFT : [self.frames[0], self.frames[1]],
+				  pg.K_RIGHT: [pg.transform.flip(self.frames[0], True, False),
+							   pg.transform.flip(self.frames[1], True, False)],
+				  pg.K_DOWN : [self.frames[3],
+							   pg.transform.flip(self.frames[3], True, False)],
+				  pg.K_UP   : [self.frames[2],
+							   pg.transform.flip(self.frames[2], True, False)]}
+		return frames
+
+	def adjust_images(self):
+		if self.direction != self.old_direction:
+			self.walkframes = self.walkframe_dict[self.direction]
+			self.old_direction = self.direction
+			self.redraw = True
+		self.make_image()
+
+	def make_image(self):
+		now = pg.time.get_ticks()
+		if self.collision_direction == None:
+			if self.redraw or now-self.animate_timer > 1000/self.animate_fps:
+				if self.direction_stack:
+					self.frame = (self.frame+1)%len(self.walkframes)
+					self.image = self.walkframes[self.frame]
+				self.animate_timer = now
+			if not self.image:
+				self.image = self.walkframes[self.frame]
+			self.redraw = False
+
+	def add_direction(self, key):
+		if key in config["direcoes"]["diretas"]:
+			if key in self.direction_stack:
+				self.direction_stack.remove(key)
+			self.direction_stack.append(key)
+			self.direction = self.direction_stack[-1]
+
+	def pop_direction(self, key):
+		if key in config["direcoes"]["diretas"]:
+			if key in self.direction_stack:
+				self.direction_stack.remove(key)
+			if self.direction_stack:
+				self.direction = self.direction_stack[-1]
+
+	def pre_update(self, obstacles):
+		self.frame_start_pos = self.rect.topleft
+
+	def check_keys(self, keys):
+		self.velocity[0] = 0
+		if keys[pg.K_LEFT] or keys[pg.K_a]:
+			self.velocity[0] -= self.speed
+		if keys[pg.K_RIGHT] or keys[pg.K_d]:
+			self.velocity[0] += self.speed
+
+	def update(self, obstaculos, keys):
+		checarPosicao(self.rect.x, self.rect.y)
+		self.adjust_images()
+		self.collision_direction = None
+		if self.direction_stack:
+			self.movement(obstaculos, 0)
+			self.movement(obstaculos, 1)
+		self.check_keys(keys)
+		start = self.frame_start_pos
+		end = self.rect.topleft
+		self.total_displacement = (end[0] - start[0], end[1] - start[1])
+
+	def movement(self, obstaculos, i):
+		change = self.speed*config["direcoes"]["diretas"][self.direction][i]
+		self.rect[i] += change
+		collisions = pg.sprite.spritecollide(self, obstaculos, False)
+		callback = pg.sprite.collide_mask
+		collide = pg.sprite.spritecollideany(self, collisions, callback)
+		if collide and not self.collision_direction:
+			self.collision_direction = self.get_collision_direction(collide)
+		while collide:
+			self.rect[i] += (1 if change < 0 else -1)
+			collide = pg.sprite.spritecollideany(self, collisions, callback)
+
+	def get_collision_direction(self, other_sprite):
+		dx = self.get_finite_difference(other_sprite, 0, self.speed)
+		dy = self.get_finite_difference(other_sprite, 1, self.speed)
+		abs_x, abs_y = abs(dx), abs(dy)
+		if abs_x > abs_y:
+			return ("right" if dx > 0 else "left")
+		elif abs_x < abs_y:
+			return ("bottom" if dy > 0 else "top")
 		else:
-			valor = 0
-		jogo[x].insert(y, valor)
+			return config["direcoes"]["opostas"][self.direction]
 
-clock = pygame.time.Clock()
+	def get_finite_difference(self, other_sprite, index, delta = 1):
+		base_offset = [other_sprite.rect.x-self.rect.x, other_sprite.rect.y-self.rect.y]
+		offset_high = base_offset[:]
+		offset_low = base_offset[:]
+		offset_high[index] += delta
+		offset_low[index] -= delta
+		first_term = self.mask.overlap_area(other_sprite.mask, offset_high)
+		second_term = self.mask.overlap_area(other_sprite.mask, offset_low)
+		return first_term - second_term
 
-def calcularPosicao(posicao):
-	return (posicao[0]*tamanhoTile+tamanhoAdicional, posicao[1]*tamanhoTile+tamanhoAdicional)
+	def draw(self, surface):
+		surface.blit(self.image, self.rect)
 
-class BordasExternas(pygame.sprite.Sprite):
-	def __init__(self, startpos):
-		pygame.sprite.Sprite.__init__(self, self.groups)
-		self.image = pygame.image.load(os.path.join(sys.path[0], "imagens/bordaExterna.png"))
-		self.pos = [0.0, 0.0]
-		self.pos[0] = startpos[0]*1.0
-		self.pos[1] = startpos[1]*1.0
-		self.image = self.image.convert_alpha()
-		self.rect = self.image.get_rect()
-		self.rect.centerx = self.pos[0]
-		self.rect.centery = self.pos[1]
-		self.area = screen.get_rect()
 
-class Bordas(pygame.sprite.Sprite):
-	def __init__(self, startpos):
-		pygame.sprite.Sprite.__init__(self, self.groups)
-		self.image = pygame.image.load(os.path.join(sys.path[0], "imagens/borda.png"))
-		self.pos = [0.0, 0.0]
-		self.pos[0] = startpos[0]*1.0
-		self.pos[1] = startpos[1]*1.0
-		self.image = self.image.convert_alpha()
-		self.rect = self.image.get_rect()
-		self.rect.centerx = self.pos[0]
-		self.rect.centery = self.pos[1]
-		self.area = screen.get_rect()
+class Tiles(pg.sprite.Sprite):
+	def __init__(self, posicao, id):
+		pg.sprite.Sprite.__init__(self)
+		self.image = config["tiles"]["tipos"][id]["arquivo"]
+		self.rect = self.image.get_rect(topleft = posicao)
+		self.mask = pg.mask.from_surface(self.image)
 
-class Tiles(pygame.sprite.Sprite):
-	def __init__(self, startpos, tipo = 1):
-		pygame.sprite.Sprite.__init__(self, self.groups)
-		# if tipo == 0:
-			# arquivo = "tile"
-		# elif tipo == 1:
-			# arquivo = "parede"
-		# elif tipo == 2:
-			# arquivo = "tile2"
-		# elif tipo == 3:
-			# arquivo = "tile3"
-		arquivo = configTiles[tipo]["nomeArquivo"]
-		self.image = pygame.image.load(os.path.join(sys.path[0], "imagens/"+arquivo+".png"))
-		self.pos = [0.0, 0.0]
-		self.pos[0] = startpos[0]*1.0
-		self.pos[1] = startpos[1]*1.0
-		self.image = self.image.convert_alpha()
-		self.rect = self.image.get_rect()
-		self.rect.centerx = self.pos[0]
-		self.rect.centery = self.pos[1]
-		self.area = screen.get_rect()
-
-class Jogador(pygame.sprite.Sprite):
+class Control(object):
 	def __init__(self):
-		pygame.sprite.Sprite.__init__(self, self.groups)
-		self.image = pygame.image.load(os.path.join(sys.path[0], "imagens/jogador.png"))
-		self.pos = [0.0, 0.0]
-		startpos = calcularPosicao(posicaoJogador)
-		self.pos[0] = startpos[0]*1.0
-		self.pos[1] = startpos[1]*1.0
-		self.image = self.image.convert_alpha()
-		self.rect = self.image.get_rect()
-		self.rect.centerx = self.pos[0]
-		self.rect.centery = self.pos[1]
-		self.area = screen.get_rect()
-	def update(self, seconds):
-		novaPos = calcularPosicao(posicaoJogador)
-		valor = 16
+		self.screen = pg.display.get_surface()
+		self.clock = pg.time.Clock()
+		self.fps = 60.0
+		self.done = False
+		self.keys = pg.key.get_pressed()
+		self.cenarioCriado = self.criarCenario()
+		self.cenario = self.cenarioCriado[0]
+		self.obstaculos = self.cenarioCriado[1]
+		self.player = Player((config["tiles"]["tamanho"]*2, config["tiles"]["tamanho"]*2, config["jogador"]["tamanho"][0], config["jogador"]["tamanho"][1]), 3)
+		self.level = pg.Surface((config["jogo"]["area"][0]*config["tiles"]["tamanho"], config["jogo"]["area"][1]*config["tiles"]["tamanho"])).convert()
+		self.level_rect = self.level.get_rect()
+		self.viewport = self.screen.get_rect(top = self.level_rect.top)
 
-		direcaox, direcaoy = 0, 0
-	
-		if self.pos[0] < novaPos[0]:
-			direcaox = 1
-		elif self.pos[0] > novaPos[0]:
-			direcaox = -1
+	def criarCenario(self):
+		cenario, obstaculos = [], []
 
-		if self.pos[1] < novaPos[1]:
-			direcaoy = 1
-		elif self.pos[1] > novaPos[1]:
-			direcaoy = -1
+		for i in range(len(config["mapa"])):
+			for j in range(len(config["mapa"][i])):
+				tipo = config["mapa"][i][j]
+				tile = config["tiles"]["tipos"][tipo]
+				tileCarregado = Tiles(calcularPosicao(j, i), tipo)
 
-		valorx = valor * direcaox
-		valory = valor * direcaoy
+				if tile["block"]:
+					obstaculos.append(tileCarregado)
 
-		if valorx != 0 or valory != 0:
-			self.rect.centerx += valorx
-			self.rect.centery += valory
-			self.pos[0] += valorx
-			self.pos[1] += valory
-		else:
-			self.rect.centerx += 0
-			self.rect.centery += 0
-			self.pos[0] += 0
-			self.pos[1] += 0
+				cenario.append(tileCarregado)
 
-bordasExternasGroup = pygame.sprite.Group()
-bordasGroup = pygame.sprite.Group()
-tilesGroup = pygame.sprite.Group()
-jogadorGroup = pygame.sprite.Group()
-allGroup = pygame.sprite.Group()
+		return [pg.sprite.Group(cenario), pg.sprite.Group(obstaculos)]
 
-BordasExternas.groups = bordasExternasGroup, allGroup
-Bordas.groups = bordasGroup, allGroup
-Tiles.groups = tilesGroup, allGroup
-Jogador.groups = jogadorGroup
+	def update_viewport(self, speed):
+		for i in (0,1):
+			first_third = self.viewport[i]+self.viewport.size[i]//3
+			second_third = first_third+self.viewport.size[i]//3
+			player_center = self.player.rect.center[i]
+			mult = 0
+			if speed[i] > 0 and player_center >= first_third:
+				mult = 0.5 if player_center < self.viewport.center[i] else 1
+			elif speed[i] < 0 and player_center <= second_third:
+				mult = 0.5 if player_center > self.viewport.center[i] else 1
+			self.viewport[i] += mult*speed[i]
+		self.viewport.clamp_ip(self.level_rect)
 
-tamanhoBorda = [
-	int(tamanhoTela[0]/tamanhoBordaExterna),
-	int(tamanhoTela[1]/tamanhoBordaExterna)
-]
+	def event_loop(self):
+		for event in pg.event.get():
+			self.keys = pg.key.get_pressed()
+			if event.type == pg.QUIT or self.keys[pg.K_ESCAPE]:
+				self.done = True
+			elif event.type == pg.KEYDOWN:
+				print(self.player.rect, self.player.rect.x)
+				self.player.add_direction(event.key)
+				# config["mapa"][4][3] = 3
+				# self.cenarioCriado = self.criarCenario()
+				# self.cenario = self.cenarioCriado[0]
+				# self.obstaculos = self.cenarioCriado[1]
+				# self.cenario = self.criarCenario()
+				# self.player.add_direction(event.key)
+			elif event.type == pg.KEYUP:
+				self.player.pop_direction(event.key)
+				# config["mapa"][4][3] = 2
+				# self.cenarioCriado = self.criarCenario()
+				# self.cenario = self.cenarioCriado[0]
+				# self.obstaculos = self.cenarioCriado[1]
+				# self.player.pop_direction(event.key)
+			# elif event.type == pg.KEYDOWN:
+				# config["mapa"][3][1] = 3
+				# self.cenario = self.criarCenario()
+				# self.player.add_direction(event.key)
+			# elif event.type == pg.KEYUP:
+				# config["mapa"][3][1] = 2
+				# self.cenario = self.criarCenario()
+				# self.player.pop_direction(event.key)
 
-for i in range(tamanhoBorda[0]):
-	for j in range(tamanhoBorda[1]):
-		if ((i == 0 or i == tamanhoBorda[0]-1) or
-			((i > 0 and i < tamanhoBorda[0]-1) and (j == 0 or j == tamanhoBorda[1]-1))):
-			posicaoBordaExterna = (i * tamanhoBordaExterna + tamanhoAdicionalBordaExterna, j * tamanhoBordaExterna + tamanhoAdicionalBordaExterna)
-			BordasExternas(posicaoBordaExterna)
+	def update(self):
+		self.keys = pg.key.get_pressed()
+		self.player.pre_update(self.obstaculos)
+		self.player.update(self.obstaculos, self.keys)
+		self.update_viewport(self.player.total_displacement)
 
-for i in range(len(jogo)):
-	for j in range(len(jogo[i])):
-		posicaoTile = calcularPosicao((i, j))
-		if jogo[i][j] == 1:
-			Bordas(posicaoTile)
-		else:
-			jogo[i][j] = mapa[j-2][i-2]
-			Tiles(posicaoTile, mapa[j-2][i-2])
+	def draw(self):
+		# self.screen.fill(config["janela"]["corFundo"])
+		# self.cenario.draw(self.screen)
+		# self.player.draw(self.screen)
+		# self.screen.fill(config["janela"]["corFundo"])
+		self.level.fill(config["janela"]["corFundo"], self.viewport)
+		# self.obstaculos.draw(self.level)
+		self.cenario.draw(self.level)
+		self.player.draw(self.level)
+		self.screen.blit(self.level, (0,0), self.viewport)
 
-Jogador()
+	def display_fps(self):
+		tituloJanela = "{} - FPS: {:.2f}".format(config["janela"]["titulo"], self.clock.get_fps())
+		pg.display.set_caption(tituloJanela)
 
-background = pygame.Surface((screen.get_width(), screen.get_height()))
-background.fill((255, 255, 255))
-background = background.convert()
+	def main_loop(self):
+		while not self.done:
+			self.event_loop()
+			self.update()
+			self.draw()
+			pg.display.update()
+			self.clock.tick(self.fps)
+			self.display_fps()
 
-def checarPosicaoBlock(posicao):
-	return configTiles[jogo[posicao[0]][posicao[1]]]["block"]
+def get_images(sheet, frame_indices, size):
+	frames = []
+	for cell in frame_indices:
+		frame_rect = ((size[0]*cell[0],size[1]*cell[1]), size)
+		frames.append(sheet.subsurface(frame_rect))
+	return frames
 
-def movimentarJogador(movimento):
-	novaPosicao = [posicaoJogador[0]+movimento[0], posicaoJogador[1]+movimento[1]]
-	if not checarPosicaoBlock(novaPosicao):
-		posicaoJogador[0] = novaPosicao[0]
-		posicaoJogador[1] = novaPosicao[1]
+def main():
+	os.environ['SDL_VIDEO_CENTERED'] = '1'
+	pg.init()
+	pg.display.set_caption(config["janela"]["titulo"])
+	definirTamanhoJanela()
+	pg.display.set_mode(config["janela"]["tamanho"])
+	carregarImagensTiles()
+	carregarImagemJogador()
+	Control().main_loop()
+	pg.quit()
+	sys.exit()
 
-while True:
-	milliseconds = clock.tick(60)
-	seconds = milliseconds / 1000.0
-
-	for e in pygame.event.get():
-		if e.type == pygame.QUIT:
-			pygame.quit()
-			quit()
-		elif e.type == pygame.KEYDOWN:
-			if e.key == pygame.K_UP:
-				movimentarJogador([0, -1])
-			elif e.key == pygame.K_DOWN:
-				movimentarJogador([0, 1])
-			elif e.key == pygame.K_RIGHT:
-				movimentarJogador([1, 0])
-			elif e.key == pygame.K_LEFT:
-				movimentarJogador([-1, 0])
-
-	screen.blit(background, (0, 0))
-
-	allGroup.clear(screen, background)
-	allGroup.update(seconds)
-	allGroup.draw(screen)
-
-	jogadorGroup.update(seconds)
-	jogadorGroup.draw(screen)
-
-	caption = "FPS: {:.2f}".format(clock.get_fps())
-	pygame.display.set_caption(caption)
-
-	pygame.display.flip()
+if __name__ == "__main__":
+	main()
